d8d0975d80fe34d4f9c0852284b4dcf65fc6241c
Mir00r <abdur.razzaq06@gmail.com>
2016-02-17 02:21:51 +0600

    how to use 1D array using java

---
+++ Collections/HackerRank/1D_Array_Easy.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n, i, j , cnt, sum;
+        n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        
+        for ( i = 0; i < n; i++ ){
+            arr[i] = sc.nextInt();
+        }
+        
+        cnt = 0;
+        for ( i = 0; i < n; i++ ){
+            sum = 0;
+            for ( j = i; j < n; j++ ){
+                sum += arr[j];
+                if (sum < 0) cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
