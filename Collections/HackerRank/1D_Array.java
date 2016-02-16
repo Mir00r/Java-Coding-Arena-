43c57a14b8c0ca586db7b94568e6eec5dad8a29f
Mir00r <abdur.razzaq06@gmail.com>
2016-02-17 02:21:22 +0600

    using 1D array solving very hard problem

--- Collections/HackerRank/1D_Array.java
+++ Collections/HackerRank/1D_Array.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -5,28 +6,69 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-
+    /*
+     int[] A;
+     int m;
+     int n;
+     boolean[] visitedArray;
+     boolean result = false;
+    */
+    
+    public static int[] A;
+    public static int m;
+    public static int n;
+    public static boolean[] visitedArray;
+    private static boolean result = false;
+    
+    
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner sc = new Scanner(System.in);
-        int n, i, j , cnt, sum;
-        n = sc.nextInt();
+        Scanner sc= new Scanner(System.in);
+        int tc = sc.nextInt();
+        int i, j;
         
-        int[] arr = new int[n];
-        
-        for ( i = 0; i < n; i++ ){
-            arr[i] = sc.nextInt();
-        }
-        
-        cnt = 0;
-        for ( i = 0; i < n; i++ ){
-            sum = 0;
-            for ( j = i; j < n; j++ ){
-                sum += arr[j];
-                
-                if (sum < 0) cnt++;
+        for ( i = 0; i < tc; i++) {
+
+            n = sc.nextInt();
+            A = new int[n];
+            visitedArray = new boolean[n];
+            Arrays.fill(visitedArray, false);
+            m = sc.nextInt();
+            
+            for ( j = 0; j < n; j++) {
+                A[j] = sc.nextInt();
             }
+            int tmp = 0;
+            result = false;
+            isPossible( tmp );
+            
+            if( result ) 
+                System.out.println("YES");
+            else 
+                System.out.println("NO");
         }
-        System.out.println(cnt);
     }
+    
+    
+       private static void isPossible( int i) {
+        if( i > n || i + m >= n || i == n - 1 ){
+            result = true;
+            return; 
+        }
+        if( visitedArray[i] == true ){
+            return;
+        }    
+        visitedArray[i]= true;
+            
+        if (i + 1 < n && A[i + 1] == 0 ){     
+           isPossible( i + 1 );                
+        }  
+        if ( i - 1 >= 0 && A[ i - 1 ] == 0){
+           isPossible( i - 1 );     
+        }
+        if( i + m < n && A[ i + m ] == 0){
+           isPossible( i + m );
+        }        
+    }
+    
 }
+
