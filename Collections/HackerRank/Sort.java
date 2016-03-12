2a1dc1453914686ca78e700f46c0e9538a392bda
Mir00r <abdur.razzaq06@gmail.com>
2016-03-13 02:31:21 +0600

    Using java Collections Sort method, sorted all student information in decreasing order

---
+++ Collections/HackerRank/Sort.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Student implements Comparable {
+   private int id;
+   private String fname;
+   private double cgpa;
+   public Student(int id, String fname, double cgpa) {
+      super();
+      this.id = id;
+      this.fname = fname;
+      this.cgpa = cgpa;
+   }
+   public int getId() {
+      return id;
+   }
+   public String getFname() {
+      return fname;
+   }
+   public double getCgpa() {
+      return cgpa;
+   }
+    
+  // Comparator<Player> desc = new Comparator<Player>(){
+       //public int compare ( Player s) {
+        public int compareTo( Object obj ) {
+           Student s = (Student)obj;
+           if (cgpa == s.cgpa){
+               if (fname.equals(s.fname))
+                   return id - s.id;
+               else
+                   return fname.compareTo(s.fname); 
+           }
+           else{
+               if( s.cgpa - cgpa > 0 )
+                   return 1;
+                else 
+                    return -1;
+            }
+        }   
+    //};
+  // };
+}
+
+//Complete the code
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      
+      List < Student > studentList = new ArrayList < Student >();
+      while( testCases > 0 ){
+         int id = in.nextInt();
+         String fname = in.next();
+         double cgpa = in.nextDouble();
+         
+         Student st = new Student(id, fname, cgpa);
+         studentList.add(st);
+         
+         testCases--;
+      }
+       
+       Collections.sort(studentList);
+       for(Student st: studentList){
+           System.out.println(st.getFname());
+       }
+   }
+}
