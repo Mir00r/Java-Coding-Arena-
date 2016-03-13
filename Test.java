9e28688e2e2052b3589614350fdbea5c2910ec43
Mir00r <abdur.razzaq06@gmail.com>
2016-03-13 20:48:19 +0600

    add iterator file

---
+++ Test.java
@@ -0,0 +1,217 @@
+[33mcommit 913e6216871c9ae46992a5830cadf10e7976b19c[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 19:58:08 2016 +0600
+
+    Practicing Java Collections framwork
+
+[33mcommit 368cc580ecec545d1ae2d868870013f033c366d8[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 19:50:45 2016 +0600
+
+    Practicing java BigDecimal Number sorting using Insertion sort
+
+[33mcommit dbc772dedfdc3043152aaf9ba234bfe8b09beefe[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 19:04:29 2016 +0600
+
+    Doing java BigInteger add and multiply operation
+
+[33mcommit 5ed59e33ce38f8c14e86273ecd54e96971d9a804[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 18:56:08 2016 +0600
+
+    Java String opearation testing code file
+
+[33mcommit eff69a9b167f27f2a972ce6a740f8d046f53a0b6[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 18:55:13 2016 +0600
+
+    Introduction of java testing cod file
+
+[33mcommit 4b781e8b11a02662ecf4cbecebc60c05fe12696d[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 18:52:31 2016 +0600
+
+    Just add simple java source file for checking my any java Biginteger code
+
+[33mcommit 54c80e1fd5bdd3e3e83322820e822e5a233801cb[m
+Merge: 82d07a9 73fbdf5
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 18:49:15 2016 +0600
+
+    Merge branch 'master' of https://github.com/Mir00r/Java-Coding-Arena-
+
+[33mcommit 82d07a9ecf096e248a7f57f703cf9d26a02da022[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 18:48:03 2016 +0600
+
+    Java BigInteger Practicing folder
+
+[33mcommit 73fbdf5c7a65f9a48a7e91de2ac2fa1427a9a5e4[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 03:47:30 2016 +0600
+
+    Tag, Content and Extractor Checking using Java
+
+[33mcommit 553887b409f94131d1d4d0d0bb1745476bcaea90[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 03:34:01 2016 +0600
+
+    How to check UserName format using Java Regualr Expression formula
+
+[33mcommit 196d45e55381af9f2875117934d567257f3fed12[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 03:27:51 2016 +0600
+
+    Using Java Regular Expression formula how to delete duplicate word
+
+[33mcommit eab6598fd511b4e4c42ce191dcf4fb691b2f07b9[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 03:03:32 2016 +0600
+
+    Regular Expression IP number checking using very different method still little bit daunt how it works
+
+[33mcommit 5938c801c3a667cc6a6a7bdee3ad7b8ba90c5eba[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 02:51:37 2016 +0600
+
+    Pattern Syntax checking using try catch function
+
+[33mcommit 599ef576de977354443be33cece708ace6783792[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 02:31:18 2016 +0600
+
+    How to Tokenizing the string using java
+
+[33mcommit ad206ae94dab68f55632ace20bf8ab8a0f24116e[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Mon Feb 15 00:54:37 2016 +0600
+
+    Checking Anagram using java
+
+[33mcommit 6cbb52a782e57788b4c61d824454fc7e102b1e72[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sun Feb 14 02:39:14 2016 +0600
+
+    how to reverse string and check palindrome
+
+[33mcommit a00752acc450e2beb46e2d14818f21d3f7ab835f[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sun Feb 14 01:10:29 2016 +0600
+
+    How compare string Lexicographic ally using java
+
+[33mcommit 72d2090285e347c5a5da667aa06ee683361a7bf6[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:11:33 2016 +0600
+
+    updated the code
+
+[33mcommit ad469ad5256211cc25015a27e4ae4c428a9228c9[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:09:19 2016 +0600
+
+    kono dorkar nai
+
+[33mcommit 5168a421b08b3594193c6d2d35164bae05533083[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:08:46 2016 +0600
+
+    kono dorkar nai
+
+[33mcommit 1f122376156e47dcc423105abccb24331e1a93d2[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:08:08 2016 +0600
+
+    kono dorkar nai
+
+[33mcommit b45d9dfa661e7229247ff99d0e6d9d9231ff2928[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:06:37 2016 +0600
+
+    i moved this file in Hackerrank folder
+
+[33mcommit 43b035f65b34d14d7ecb078add23c3903e139158[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:05:49 2016 +0600
+
+    hm at last i deleted this file
+
+[33mcommit 294efc7bc785bad5574e0971dbb612fde87662d8[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:04:21 2016 +0600
+
+    dorkar nai
+
+[33mcommit 93e2fee5fc4b975d0793bd8f7b8598c13ef6117b[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:02:52 2016 +0600
+
+    i have another file
+
+[33mcommit 47f021cdd00ff7a87c545ff17e68869d93a14841[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:01:03 2016 +0600
+
+    found another duplicate file
+
+[33mcommit d9ea820cb5522ae6b2d6a3f5ae7d59888bb9a042[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 15:00:02 2016 +0600
+
+    it's a duplicate file
+
+[33mcommit 089789af3eb5fccc1d925182f666c5388f3f9d84[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 14:58:57 2016 +0600
+
+    no need to worry about that
+
+[33mcommit f215266440b76fc8d3d5434d13732f124bb9a326[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 14:51:27 2016 +0600
+
+    add new folder for making file organized ver well
+
+[33mcommit 214d437775dd762919ce96e4e0029fec08d76c3a[m
+Merge: e3ae232 d8cfd97
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 14:46:06 2016 +0600
+
+    Merge branch 'master' of https://github.com/Mir00r/Java-Coding-Arena-
+
+[33mcommit e3ae2324d6d599bc546f8bf32d7a1f9797054d7e[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 14:41:54 2016 +0600
+
+    add all files for practicing String using Java
+
+[33mcommit 47bfedb8dabcfc38ea6080f7046c0d2756d31ff5[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Sat Feb 13 14:34:12 2016 +0600
+
+    for string operation
+
+[33mcommit d8cfd979d1b9bc5b3ce00cc70ac450e2d25aff82[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Wed Feb 10 02:44:26 2016 +0600
+
+    how to convert String to int number
+
+[33mcommit 006edcdcead3163421e88a62baf6465de95ee51e[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Wed Feb 10 02:38:51 2016 +0600
+
+    how initialize ta static block
+
+[33mcommit 6f6762e271e393e161ab30c692c1b003af7b86c6[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Wed Feb 10 02:15:06 2016 +0600
+
+    Java End Of file checking
+
+[33mcommit 67a1047dc9487a5454ec95ffdc0bc780dcc54e33[m
+Author: Mir00r <abdur.razzaq06@gmail.com>
+Date:   Wed Feb 10 02:05:35 2016 +0600
+
+    Checking Java all data types length using try catch function
